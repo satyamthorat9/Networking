7c6f7b232513923dee752d3f44817989a52c7333
satyamthorat9 <satyamthorat29@gmail.com>
2026-04-27 14:42:16 +0530

    update

--- msg_encript.c
+++ msg_encript.c
@@ -1,13 +1,32 @@
-#include <stdio.h>
+// #include <stdio.h>
 
+// int main() {
+//     char msg[100];
+//     int i;
+
+//     printf("Enter message: ");
+//     scanf("%s", msg);
+
+//     for(i = 0; msg[i] != '\0'; i++) {
+//         msg[i] = msg[i] + 3; // Caesar Cipher
+//     }
+
+//     printf("Encrypted Message: %s\n", msg);
+
+//     return 0;
+// }
+
+
+#include <stdio.h>
+#include<string.h>
 int main() {
     char msg[100];
     int i;
 
     printf("Enter message: ");
-    scanf("%s", msg);
+    scanf("%[^\n]", msg);
 
-    for(i = 0; msg[i] != '\0'; i++) {
+    for(i = 0; i< strlen(msg); i++) {
         msg[i] = msg[i] + 3; // Caesar Cipher
     }
 
